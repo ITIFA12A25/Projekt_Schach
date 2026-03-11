09cf947f4657361acc7e5c1445a65c56bc9f7c46
ARichardson-bs <alexander.richardson@bs-erlangen.de>
2026-03-11 11:12:02 +0100

    Fixed move logic for some pices
    variable was pointer instead of new object in rook
    queen fixed move check logic

--- RestApiChess/Rook.cpp
+++ RestApiChess/Rook.cpp
@@ -10,18 +10,28 @@ bool Rook::canMove(const Board &board, Position *from, Position *to) const {
     int stepX = (dx == 0) ? 0 : (dx > 0 ? 1 : -1);
     int stepY = (dy == 0) ? 0 : (dy > 0 ? 1 : -1);
 
-    Position *p = from;
+    Position p;
+    p.x = from->x;
+    p.y = from->y;
+
     const Cell &dest = board.cellAt(to);
-    const Cell  &src = board.cellAt(from);
+    const Cell &src  = board.cellAt(from);
+
     while (true) {
-        p->x += stepX;
-        p->y += stepY;
-        if (p->x == to->x && p->y == to->y) break;
-        if (board.cellAt(p).isFilled() && src.isFilled())
+        p.x += stepX;
+        p.y += stepY;
+
+        if (p.x == to->x && p.y == to->y) break;
+
+        if (board.cellAt(&p).isFilled() && src.isFilled()) {
             return false;
+        }
     }
+
 
     if (!dest.isFilled() && src.isFilled())
         return true;
-    return dest.isFilled() && src.isFilled() && dest.piece->isWhite() != src.piece->isWhite();
+    bool isSameColor =  dest.piece->isWhite() == src.piece->isWhite();
+    bool positionsValid = dest.isFilled() && src.isFilled();
+    return positionsValid && !isSameColor;
 }
